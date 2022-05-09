3802b625696dc53f7f9531f6757224a5375a12a8
Nick Jones <nickj_24@hotmail.com>
2022-05-09 14:13:29 +0100

    update to lab work

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,1 +1,10 @@
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
 
+    def student_has_name(self, name):
+        return name
+
+    def student_has_cohort(self, cohort):
+        return cohort
